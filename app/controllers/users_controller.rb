9d6322bc4ba5d28cb3c535e328e6fdfeec10ef60
Lyubomyr <fedorniakl@gmail.com>
2012-09-26 16:53:29 +0300

    add registrational form and integrational tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
+  
+  def index
+  	@users = User.all
+  end
+
   def new
   	@title = "Sign up"
+  	@user = User.new
   end
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "User successfuly created"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
